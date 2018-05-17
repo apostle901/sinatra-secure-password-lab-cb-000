d1ce03eab9b68ac78eaa444e80f34c9245e1e4ce
Jawann <jawann@gmail.com>
2018-05-17 10:30:05 +0000

    automatically backed up by learn

--- db/migrate/20180505024033_setup_user_accounts.rb
+++ db/migrate/20180505024033_setup_user_accounts.rb
@@ -2,7 +2,7 @@ class SetupUserAccounts < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      t.string :password_digest
       t.decimal :account
     end
   end
